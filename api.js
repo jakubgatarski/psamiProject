d6f28353d1172d039a775d6d0e21895cb61aed2b
jakubgatarski <jakubgat1@gmail.com>
2022-09-04 17:20:06 +0200

    basic CRUD methods

---
+++ api.js
@@ -0,0 +1,71 @@
+//node app.js ---------------------------------------------------------------------------------
+const client = require('./connection.js')
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.listen(4000, ()=>{
+    console.log("Sever is now listening at port 4000");
+})
+
+client.connect();
+app.get('/users', (req, res)=>{
+    client.query(`Select * from users`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.get('/users/:id', (req, res)=>{
+    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.post('/users', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `insert into users(id, first_name, last_name) 
+                       values(${user.id}, '${user.first_name}', '${user.last_name}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+app.put('/users/:id', (req, res)=> {
+    let user = req.body;
+    let updateQuery = `update users
+                       set first_name = '${user.first_name}',
+                       last_name = '${user.last_name}'
+                       where id = ${user.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+app.delete('/users/:id', (req, res)=> {
+    let insertQuery = `delete from users where id=${req.params.id}`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
